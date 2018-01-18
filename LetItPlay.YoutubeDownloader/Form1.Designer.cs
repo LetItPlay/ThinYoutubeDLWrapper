b5498eaca159d9413ee9df1651e71279f721f816
opcheese <opcheese@gmail.com>
2018-01-18 15:27:03 +0500

    initial commit

---
+++ LetItPlay.YoutubeDownloader/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace LetItPlay.YoutubeDownloader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textUrl = new System.Windows.Forms.TextBox();
+            this.btnLanuch = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textUrl
+            // 
+            this.textUrl.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textUrl.Location = new System.Drawing.Point(25, 45);
+            this.textUrl.Name = "textUrl";
+            this.textUrl.Size = new System.Drawing.Size(621, 20);
+            this.textUrl.TabIndex = 0;
+            // 
+            // btnLanuch
+            // 
+            this.btnLanuch.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnLanuch.Location = new System.Drawing.Point(571, 235);
+            this.btnLanuch.Name = "btnLanuch";
+            this.btnLanuch.Size = new System.Drawing.Size(75, 23);
+            this.btnLanuch.TabIndex = 1;
+            this.btnLanuch.Text = "Начать закачку";
+            this.btnLanuch.UseVisualStyleBackColor = true;
+            this.btnLanuch.Click += new System.EventHandler(this.btnLanuch_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(25, 92);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "url видео или канала";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(120, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "дата с которой начать";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(658, 283);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.btnLanuch);
+            this.Controls.Add(this.textUrl);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textUrl;
+        private System.Windows.Forms.Button btnLanuch;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ LetItPlay.YoutubeDownloader/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+using Newtonsoft.Json.Linq;
+
+namespace LetItPlay.YoutubeDownloader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public static string[] GetFiles(string path, string searchPattern, SearchOption searchOption = SearchOption.TopDirectoryOnly)
+        {
+            string[] searchPatterns = searchPattern.Split('|');
+            List<string> files = new List<string>();
+            foreach (string sp in searchPatterns)
+                files.AddRange(System.IO.Directory.GetFiles(path, sp, searchOption));
+            files.Sort();
+            return files.ToArray();
+        }
+        private void btnLanuch_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(this.textUrl.Text))
+            {
+                MessageBox.Show("url empty");
+                return;
+            }
+            DateTime dt = this.dateTimePicker1.Value;
+            string dat = dt.ToString("yyyyMMdd");
+            string url = this.textUrl.Text;
+            //--download-archive archive.txt
+            string cmd = String.Format(
+                "  --dateafter {0} -o \"{2}/%(title)s.%(ext)s\" --write-info-json  -f \"worstvideo + bestaudio[ext = mp3] / best[ext = mp3] / best\" -x --audio-format mp3 {1}", 
+                dat,url, Application.StartupPath + "\\Downloaded");
+           
+            string processName = "dl/youtube-dl.exe";
+            Directory.CreateDirectory("Downloaded");
+
+            string[] files = GetFiles("Downloaded", "*.mp3|*.json");
+            for (var i = files.Length - 1; i >= 0; i--)
+            {
+                File.Delete(files[i]);
+            }
+
+            var process = Process.Start(Application.StartupPath + "\\"+processName,cmd);
+            process.WaitForExit();
+            ProcessJsons("Downloaded\\");
+
+            Process.Start(@"Downloaded\\");
+        }
+
+        public void ProcessJsons (string path)
+        {
+            string[] files = GetFiles("Downloaded", "*.json");
+            for (var i = files.Length - 1; i >= 0; i--)
+            {
+                var inpJson = File.ReadAllText(files[i]);
+                var outJson =ExtractJson(inpJson);
+                File.Delete(files[i]);
+                File.WriteAllText(files[i],outJson);
+            }
+
+        }
+
+        public string ExtractJson(string jsonInput) {
+            string res="";
+            JObject jInput = JObject.Parse(jsonInput);
+            JObject jOUt = new JObject();
+            jOUt.Add("url",jInput.GetValue("url"));
+            jOUt.Add("title", jInput.GetValue("title"));
+            jOUt.Add("thumbnail", jInput.GetValue("thumbnail"));
+            jOUt.Add("fulltitle", jInput.GetValue("fulltitle"));
+            JArray tags = new JArray(jInput.GetValue("tags"));
+            jOUt.Add("tags", tags);
+
+            return jOUt.ToString();
+        }
+    }
+}
